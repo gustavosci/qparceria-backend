36ea5bf03226200cdffd6de9b18f64191f72e08c
Gustavo Santos <gustavosci@live.com>
2018-04-05 14:07:28 -0300

    Testando a primeira classe de dominio - User

--- src/main/java/br/com/qparceria/resources/UserResource.java
+++ src/main/java/br/com/qparceria/resources/UserResource.java
@@ -1,16 +1,27 @@
 package br.com.qparceria.resources;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.qparceria.domain.User;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 
 	@RequestMapping(method=RequestMethod.GET)	
-	public String listar() {
-		return "REST Ok :)";
+	public ArrayList<User> listar() {
+		User user1 = new User(1, "Gustavo Santos", "gustavo", "gustavosci@Live.com", "123456");
+		User user2 = new User(2, "Daniela Morais", "dani", "danielamorais@Live.com", "789");
+		
+		ArrayList<User> list = new ArrayList<User>();
+		list.add(user1);
+		list.add(user2);
+		
+		return list;
 	}
 	
 }
