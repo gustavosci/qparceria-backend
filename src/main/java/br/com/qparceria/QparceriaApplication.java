66688b5fadaa8c98e734b6ee60e2cbf47fb9fdb6
Gustavo Santos <gustavosci@live.com>
2018-04-05 15:14:51 -0300

    Criada operação de instanciação para teste de  User

--- src/main/java/br/com/qparceria/QparceriaApplication.java
+++ src/main/java/br/com/qparceria/QparceriaApplication.java
@@ -1,12 +1,33 @@
 package br.com.qparceria;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class QparceriaApplication {
+import br.com.qparceria.domain.User;
+import br.com.qparceria.repositories.UserRepository;
 
+@SpringBootApplication
+public class QparceriaApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepo; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(QparceriaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		User user1 = new User(null, "Gustavo Santos", "gustavo", "gustavosci@Live.com", "123456");
+		User user2 = new User(null, "Daniela Morais", "dani", "danielamorais@Live.com", "789");
+		ArrayList<User> users = new ArrayList<>();
+		users.add(user1);
+		users.add(user2);
+		userRepo.saveAll(users);
 	}
 }
