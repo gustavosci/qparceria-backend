8d0a08483ce90178848ef52a8c965dfc0e30286a
Gustavo Santos <gustavosci@live.com>
2018-04-11 19:24:34 -0300

    Configuração inicial do Spring Security

--- src/main/java/br/com/qparceria/resources/UserResource.java
+++ src/main/java/br/com/qparceria/resources/UserResource.java
@@ -9,7 +9,6 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +22,6 @@ import br.com.qparceria.dto.UserDTO;
 import br.com.qparceria.services.UserService;
 
 @RestController
-@CrossOrigin
 @RequestMapping(value="/users")
 public class UserResource {
 
