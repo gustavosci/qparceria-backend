d2c1f4a2a2103a3b9f7fea9263313e9ef693317f
Gustavo Santos <gustavosci@live.com>
2018-04-12 20:58:33 -0300

    Implementado refresh token

---
+++ src/main/java/br/com/qparceria/resources/AuthResource.java
@@ -0,0 +1,31 @@
+package br.com.qparceria.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.qparceria.security.JWTUtil;
+import br.com.qparceria.security.UserSS;
+import br.com.qparceria.services.UserLoggedService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse res){
+		UserSS user = UserLoggedService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		res.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+}
