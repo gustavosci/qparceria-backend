30c95f28ceceff66feaf42f41d23dedeccd6c9e9
Gustavo Santos <gustavosci@live.com>
2018-05-09 18:36:18 -0300

    Inserida validação para cidade inicial/final e esporte não informados

--- src/main/java/br/com/qparceria/services/validations/ActivitySaveValidator.java
+++ src/main/java/br/com/qparceria/services/validations/ActivitySaveValidator.java
@@ -2,43 +2,34 @@ package br.com.qparceria.services.validations;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.HandlerMapping;
-
 import br.com.qparceria.dto.ActivityDTO;
-import br.com.qparceria.repositories.ActivityRepository;
 import br.com.qparceria.resources.exceptions.FieldMessage;
 
 public class ActivitySaveValidator implements ConstraintValidator<ActivitySave, ActivityDTO> {
 
-	@Autowired
-	private ActivityRepository repoAct;
-	@Autowired
-	private HttpServletRequest req;
-	
 	@Override
 	public void initialize(ActivitySave ann) {
 	}
  
 	@Override
 	public boolean isValid(ActivityDTO objDto, ConstraintValidatorContext context) {
-		@SuppressWarnings("unchecked")
-		Map<String, String> map = (Map<String, String>) req.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
-		Integer uriId = 0;
-		if (map.get("id") != null) {
-			uriId = Integer.parseInt(map.get("id"));
-		}		
-
-		// Lista de erros
 		List<FieldMessage> list = new ArrayList<>();
 		
-		// Inserir validações aqui, quando existir
+		if(objDto.getCityStartId() <= 0) {
+			list.add(new FieldMessage("Cidade inicial", "Cidade inicial não informada"));
+		}
+		
+		if(objDto.getCityEndId() <= 0) {
+			list.add(new FieldMessage("Cidade final", "Cidade final não informada"));
+		}
+
+		if(objDto.getSportId() <= 0) {
+			list.add(new FieldMessage("Esporte", "Esporte não informado"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
